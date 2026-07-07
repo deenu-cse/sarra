a0a295992e579346899f10c94f5eb7d854abcc6b
deenu-cse <236301074@gkv.ac.in>
2026-07-07 22:24:41 +0530

    update image assets: replace JPEGs with PNGs and add new image

--- src/components/bhagirath/BhagirathPage.jsx
+++ src/components/bhagirath/BhagirathPage.jsx
@@ -3,10 +3,10 @@
 import React from 'react';
 
 const IMG = {
-    hero: '/assets/bhagirithi/SARRA_3.jpeg',
-    infoImage: '/assets/bhagirithi/SARRA_3.jpeg',
-    ui1: '/assets/bhagirithi/bhg1.jpeg',
-    ui2: '/assets/bhagirithi/bhg2.jpeg',
+    hero: '/assets/bhagirithi/SARRA_3.png',
+    infoImage: '/assets/bhagirithi/SARRA_3.png',
+    ui1: '/assets/bhagirithi/bhg1.png',
+    ui2: '/assets/bhagirithi/bhg2.png',
 };
 
 function HeroSection() {
